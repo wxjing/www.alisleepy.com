95ba89955a59d4694fd67dd66625cf6dbaeff1a0
v_wangxiaojing <v_wangxiaojing@baidu.com>
2020-03-28 14:24:25 +0800

    go module

--- controllers/Setting.go
+++ controllers/Setting.go
@@ -1,23 +1,26 @@
 package controllers
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 	. "www.alisleepy.com/models"
 	//"fmt"
 )
-//获取个人信息和浏览次数
-func GetMyInfo(this *gin.Context){
+
+//GetMyInfo 获取个人信息和浏览次数
+func GetMyInfo(this *gin.Context) {
 	myInfo := GetMyInfosAndViewNum()
 	this.JSON(http.StatusOK, gin.H{
-		"code":200,
-		"data":myInfo,
+		"code": 200,
+		"data": myInfo,
 	})
 }
-//获取友链
-func GetFriendlyUrl(this *gin.Context){
+
+//GetFriendlyUrl 获取友链
+func GetFriendlyUrl(this *gin.Context) {
 	urls := GetFriendlyUrls()
 	this.JSON(http.StatusOK, gin.H{
-		"data" : urls,
+		"data": urls,
 	})
 }
